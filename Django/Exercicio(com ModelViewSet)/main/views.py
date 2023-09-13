0c852eeca93b3e350eb8ad637f869260c2ddd35d
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2023-09-12 22:35:48 -0300

    update

---
+++ Django/Exercicio(com ModelViewSet)/main/views.py
@@ -0,0 +1,28 @@
+
+from .models import *
+from .serializer import *
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+class CharactersAPIView(ModelViewSet):
+    queryset = Characters.objects.all()
+    serializer_class = CharactersSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'status', 'origin'] #campos que podem ser filtrados filtrar
+    permission_classes =(IsAuthenticated,)
+    
+class EpisodeAPIView(ModelViewSet):
+    queryset = Episode.objects.all()
+    serializer_class = EpisodeSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'episode'] #campos que podem ser filtrados filtrar
+        
+    
+class LocationAPIView(ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    filter_backends = [DjangoFilterBackend] #biblioteca para filtro
+    filterset_fields = ['name','dimension'] #campos que podem ser filtrados filtrar
+    
