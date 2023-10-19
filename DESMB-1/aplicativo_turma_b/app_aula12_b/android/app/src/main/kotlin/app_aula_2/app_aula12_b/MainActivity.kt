e5bbc271cc7398b6ae421941ef101a46582187d7
Eduardo <114110428+RaphaelRSantos02@users.noreply.github.com>
2023-10-18 22:07:38 -0300

    new files

---
+++ DESMB-1/aplicativo_turma_b/app_aula12_b/android/app/src/main/kotlin/app_aula_2/app_aula12_b/MainActivity.kt
@@ -0,0 +1,6 @@
+package app_aula_2.app_aula12_b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ DESMB-1/aplicativo_turma_b/app_tarefa/android/app/src/main/kotlin/app_tarefas/app_tarefa/MainActivity.kt
@@ -0,0 +1,6 @@
+package app_tarefas.app_tarefa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
