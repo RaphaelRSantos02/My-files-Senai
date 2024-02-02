793082e5c62d8d9881b25a82c8a86bb2339d42c5
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-02-01 21:36:24 -0300

    conversationId

--- Chatbot/main/views.py
+++ Chatbot/main/views.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response
 from bardapi import BardCookies
 import requests
 
-psid = ""
-psidts = ""
-psidcc = ""
+psid = "g.a000gAikfuDWYsLay0sLjWQ1ZHExOEET68My8jxveoQIiL9Mal5GvafvYK0u9Eg4LZMAN1GdgQACgYKAXASAQASFQHGX2Mi8gKKK5bW9qOHAD46jIqqqxoVAUF8yKrHsCWJQNE7a9uElcTe3Ds70076"
+psidts = "sidts-CjEBPVxjShR72qmZvzmOjROVUtUpiJxOYEHSFom9dfM0xwMpkEnUghwH5_3jpvV_7YJBEAA"
+psidcc = "ABTWhQGN-NsVDAVTdpUeWIWYv98pofPC3HaPht-rWpJsA8JPOK2gRxW-RHHt0jGI-QiCUWwc9Yc"
 #cria um conjunto com os tokens de autenticação
 #para poder usar o Bard
 tokenCookies = {
@@ -24,9 +24,17 @@ class ChatBotAPIView(APIView):
         #pega os dados que veio na requisição
         data = request.data
 
-        print("data received:")
-        print(data['question'])
+        #pega o dados da conversationId caso ele seja informado para mander a mesma conversa com o chatbot
+        conversationId = data.get("conversationId")
 
+        #verifica se o id da conversa foi recebido
+        if(conversationId is not None):
+
+            #informa o bard para responder na conversa desejada
+            bard.conversation_id = conversationId
+        else:
+            bard.conversation_id = None
+        
         answer = bard.get_answer(data['question'])
 
         return Response(status=201,data=answer)
