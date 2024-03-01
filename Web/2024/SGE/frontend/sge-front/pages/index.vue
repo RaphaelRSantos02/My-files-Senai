88ecc18fb49ba67103bfe4d69ddf050223364438
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-02-29 22:48:34 -0300

    SGE

---
+++ Web/2024/SGE/frontend/sge-front/pages/index.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="login_main">
+        <section class = "logo_panel">
+            <img src="/sge.jpeg" alt="Logo do projeto sge">
+            <h1>Sistema de Gestão de Ensino</h1>
+        </section>
+        <section class="login_panel">
+
+        </section>
+
+    </main>
+        
+</template>
+
+<style lang="scss">
+    .login_main{
+        width: 100vw;
+        height: 100vh;
+        background-color: var(--ligh-background-color);
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+
+        .logo_panel{
+            display: none;
+        }
+        .login_panel{
+            width: 100vw;
+            height: 100vh;
+        }
+    }
+
+    @media screen and (min-width: 600px) {
+        .login_main{
+            .logo_panel{
+                width: 50vw;
+                display: flex;
+            }
+            .login_panel{
+                width: 50vw;
+            }
+        }
+        
+    }
+
+</style>
