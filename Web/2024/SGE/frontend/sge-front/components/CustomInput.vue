4c987c7f498b5f94f91c500f26d08a4f10a65d09
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-03-04 20:44:06 -0300

    customInput

---
+++ Web/2024/SGE/frontend/sge-front/components/CustomInput.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps({
+    label: {type: String, required: true},
+    type: {type: String, default: 'text'},
+    inputId: {type: String, required: true, default: 'custom_input'}
+});
+
+const model = defineModel();
+
+</script>
+
+<template>
+    <div class="custom_input_field">
+        <label :for="inputId">{{ props.label }}</label>
+        <input v-model="model" :id="inputId" :type="props.type">
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+    .custom_input_field{
+        overflow-y: hidden;
+        label{
+            width: 100%;
+            color: black;
+        }
+        label:focus,
+        label:valid,
+        input:focus,
+        input:valid{
+            outline: 0;
+            border-color: crimson;
+        }
+
+        input{
+            width: 100%;
+            padding: 10px;
+            border: 0;
+            border-bottom: 2px solid black;
+            font-size: 20px;
+        }
+    }
+</style>
--- Web/2024/SGE/frontend/sge-front/pages/index.vue
+++ Web/2024/SGE/frontend/sge-front/pages/index.vue
@@ -1,14 +1,40 @@
 <script setup lang="ts">
-
+    const credentials = reactive({
+        username: '',
+        password: ''
+    })
 </script>
 
 <template>
-    <main class="login_main">
-        <section class = "logo_panel">
+    <main class="flex_center login_main">
+        <section class = "logo_panel flex_center">
             <img src="/sge.jpeg" alt="Logo do projeto sge">
             <h1>Sistema de Gestão de Ensino</h1>
+            <p> &reg; Senai Roberto Mange</p>
+            
+            <div class="stars1"></div>
+            <div class="stars2"></div>
+            <div class="stars3"></div>
+
         </section>
         <section class="login_panel">
+            <div class="login_content flex_center">
+                <form class="login_form">
+                    <div class="input_container">
+                        <CustomInput label="LOGIN" inputId="user_login"
+                        v-model="credentials.username"
+                        />
+                    </div>
+                    <div class="input_container">
+                        <CustomInput label="SENHA" type="password" inputId="pass_login"
+                        v-model="credentials.password"
+                        />
+                        
+                        
+                    </div>
+                    <button type="submit" class="customButton">ENTRAR</button>
+                </form>
+            </div>
 
         </section>
 
@@ -17,35 +43,53 @@
 </template>
 
 <style lang="scss">
-    .login_main{
-        width: 100vw;
-        height: 100vh;
-        background-color: var(--ligh-background-color);
+
+    .flex_center{
         display: flex;
         flex-direction: row;
         align-items: center;
         justify-content: center;
+    }
+
+
+    .login_main{
+        width: 100vw;
+        height: 100vh;
+        background-color: var(--dark-background-color);
+
 
         .logo_panel{
             display: none;
+            width: 50vw;
+            height: 100vh;
+            flex-direction: column;
+            color: var(--light-background-color);
+            img{
+                margin: 0px 0px 25px 0px; 
+                width: 65%;
+                height: 45%;
+            }
+            
         }
+
         .login_panel{
             width: 100vw;
-            height: 100vh;
+            height: 100vh;  
+            background-color: var(--light-background-color);
         }
     }
 
+
     @media screen and (min-width: 600px) {
         .login_main{
-            .logo_panel{
-                width: 50vw;
-                display: flex;
-            }
             .login_panel{
                 width: 50vw;
             }
-        }
+            .logo_panel{
+                display: flex;
+            }
         
+        }
     }
 
 </style>
