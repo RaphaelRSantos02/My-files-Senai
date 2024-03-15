55b899a496156ac7b56aee082672760e5e869969
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-03-14 22:11:21 -0300

    updates

---
+++ Web/2024/Exercicio/frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Medias visuais: 
+        <Select>
+            <option value="">Youtube</option>
+            <option value="">Twitch</option>
+            <option value="">Amazon</option>
+            <option value="">Netflix</option>
+        </Select>
+    
+    </div>
+</template>
+
+<script>
+
+
+</script>
