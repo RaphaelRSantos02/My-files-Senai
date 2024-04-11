88e625bac6e3a838dec1fe978b4d22bc5857af3b
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-04-11 19:24:08 -0300

    deploy

--- Web/2024/SGE/backend/sge_backend/settings.py
+++ Web/2024/SGE/backend/sge_backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1ir!juv#mrms_x1&upw%#3kkjkp55aqxyrky5l7yk4tlvr05%n'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,7 +91,7 @@ WSGI_APPLICATION = 'sge_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': env('ENGINE'),
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
