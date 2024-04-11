fee8e61a13a5300c045274021872cb061b8d0838
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-04-11 20:39:37 -0300

    Update settings.py

--- Web/2024/SGE/backend/sge/settings.py
+++ Web/2024/SGE/backend/sge/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-prodution_server = 'https://sge-backend-production.up.railway.app/'
+prodution_server = 'sge-backend-production.up.railway.app/'
 
 ALLOWED_HOSTS = [
     'http://localhost',
