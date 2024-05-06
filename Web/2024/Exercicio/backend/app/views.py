073b734072158c9bb2532145ae0007998bd527e5
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-05-06 20:33:55 -0300

    Update views.py

--- Web/2024/Exercicio/backend/app/views.py
+++ Web/2024/Exercicio/backend/app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+
+class CategoriaProdutosView(ModelViewSet):
+    queryset = CategoriaProdutos.objects.all() #select *from CategoriaProdutos
+    serializer_class = CategoriaProdutosSerializer
+
+class ProdutosView(ModelViewSet):
+    queryset = Produtos.objects.all() 
+    serializer_class = ProdutosSerializer
+
+class VendasView(ModelViewSet):
+    queryset = Vendas.objects.all() 
+    serializer_class = VendasSerializer
+
+class VendasProdutosView(ModelViewSet):
+    queryset = VendasProdutos.objects.all() 
+    serializer_class = VendaProdutosSerializer
