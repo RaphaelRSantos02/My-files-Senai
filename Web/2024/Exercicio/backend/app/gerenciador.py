9b18f9416fe2ec4e1694f65c320c16c2541749ee
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-05-06 19:43:58 -0300

    recreate

--- Web/2024/Exercicio/backend/app/gerenciador.py
+++ Web/2024/Exercicio/backend/app/gerenciador.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import BaseUserManager
 
 class Gerenciador(BaseUserManager):
+
     def create_user(self, email, password=None, cpf=None, **extra_fields):
+        
         email = self.normalize_email(email)
         user = self.model(
             email=email,
@@ -11,8 +13,9 @@ class Gerenciador(BaseUserManager):
         user.set_password(password)
         user.save(using=self.db)
         return user
-    def create_superuser(self, email, password=None, cpf=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+
+    def create_superuser(self, email, password=None, cpf='08453757259', **extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
 
         return self.create_user(email, password, cpf, **extra_fields)
