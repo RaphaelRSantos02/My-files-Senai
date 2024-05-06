d4ed8473b6483ff27605e9acb12fb5d1802e9f23
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-05-06 20:33:58 -0300

    Update settings.py

--- Web/2024/Exercicio/backend/bebidasOnline/settings.py
+++ Web/2024/Exercicio/backend/bebidasOnline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s3q(weedq-_d37=4qk2tzf(7!-+ksly2-*m_#$y4=f)wyh!j-x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'app'
 ]
 
@@ -46,12 +49,15 @@ AUTH_USER_MODEL = 'app.CustomUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'bebidasOnline.urls'
 
