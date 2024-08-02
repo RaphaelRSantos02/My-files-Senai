2c11278e306a2112c504992d4c04fcb5f34c84b2
Murilo Alves <Raphar_santos@hotmail.com>
2024-08-01 22:21:16 -0300

    WEB

---
+++ Web/2024/Segundo semestre/mange-bot/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <main id="card-app">
+    <div id="content-app">
+      <header>
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/buildView"> Build </RouterLink>
+            <RouterLink to="cartView"> Carrinho </RouterLink>
+        </nav>
+      </header>
+    <RouterView/>
+    </div>
+  </main>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ Web/2024/Segundo semestre/mange-bot/src/views/buildView.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        <h1>Build</h1>
+    </main>
+</template>
