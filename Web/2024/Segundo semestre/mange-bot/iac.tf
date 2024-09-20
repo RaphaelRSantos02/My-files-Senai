5357c6c1ea8d5470c9acfced1b162d3af416c633
RaphaelRSantos02 <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-09-19 22:00:28 -0300

    Update iac.tf

--- Web/2024/Segundo semestre/mange-bot/iac.tf
+++ Web/2024/Segundo semestre/mange-bot/iac.tf
@@ -9,30 +9,30 @@ terraform {
 
 provider "azurerm" {
     resource_provider_registrations = "none"
-    subscription_id = "481c1d25-e4df-4209-9f20-6d3c0c28a214"
+    subscription_id = "250ae9c3-6c33-4030-b72a-ed22fce22920"
     features {
       
     } 
 }
 
-resource "azurerm_resource_group" "raphael-robot-t-group" {
-  name = "raphael-robot-t-group"
-  location = "eastus2"
+resource "azurerm_resource_group" "raphael-robot-t5-group" {
+  name = "raphael-robot-t5-group"
+  location = "Brazil South"
 }
 
-resource "azurerm_service_plan" "raphael-robot-t-sp" {
-  name = "raphael-robot-t-sp"
-  resource_group_name = azurerm_resource_group.raphael-robot-t-group.name
-  location = azurerm_resource_group.raphael-robot-t-group.location
-  sku_name = "B1"
+resource "azurerm_service_plan" "raphael-robot-t5-sp" {
+  name = "raphael-robot-t5-sp"
+  resource_group_name = azurerm_resource_group.raphael-robot-t5-group.name
+  location = azurerm_resource_group.raphael-robot-t5-group.location
+  sku_name = "S1"
   os_type = "Windows"
 }
 
-resource "azurerm_windows_web_app" "raphael-robot-t-app" {
-    name = "raphael-robot-t-app"
-    resource_group_name = azurerm_resource_group.raphael-robot-t-group.name
-    location = azurerm_resource_group.raphael-robot-t-group.location
-    service_plan_id = azurerm_service_plan.raphael-robot-t-sp.id
+resource "azurerm_windows_web_app" "raphael-robot-t5-app" {
+    name = "raphael-robot-t5-app"
+    resource_group_name = azurerm_resource_group.raphael-robot-t5-group.name
+    location = azurerm_resource_group.raphael-robot-t5-group.location
+    service_plan_id = azurerm_service_plan.raphael-robot-t5-sp.id
     site_config {
       always_on = false
     }
